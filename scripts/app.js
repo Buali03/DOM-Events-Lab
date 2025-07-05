b573248c734552c4ef4cf9ad9ae59bb23d3913bd
Your Name <you@example.com>
2025-07-05 10:44:34 +0300

    Submitting calculator

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,60 @@
 function init() {
-    let num1
-    let num2
-    let operation
+  let num1 = "";
+  let num2 = "";
+  let operator = "";
+  let result;
 
-    const buttonNum = document.querySelectorAll('button number')
-    function num1Value(){
-        num1 += buttonNum.innerText
+  const numElem = document.querySelectorAll(".button.number");
+  const operatorElem = document.querySelectorAll(".button.operator");
+  const displayElem = document.querySelector(".display");
+  const equalsElem = document.querySelector(".button.equals");
+
+  numElem.forEach((element) => {
+    element.addEventListener("click", function () {
+      if (operator === "") {
+        num1 += element.textContent;
+        displayElem.textContent = num1;
+      } else {
+        num2 += element.textContent;
+        displayElem.textContent = num2;
+      }
+    });
+  });
+
+  operatorElem.forEach((element) => {
+    element.addEventListener("click", function () {
+      if (element.textContent === "C") {
+        num1 = "";
+        num2 = "";
+        operator = "";
+        displayElem.textContent = operator;
+      } else {
+        operator = element.textContent;
+        displayElem.textContent = operator;
+      }
+    });
+  });
+
+  equalsElem.addEventListener("click", function () {
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    if (operator === "/") {
+      result = num1 / num2;
+      displayElem.textContent = result;
+    } else if (operator === "*") {
+      result = num1 * num2;
+      displayElem.textContent = result;
+    } else if (operator === "-") {
+      result = num1 - num2;
+      displayElem.textContent = result;
+    } else {
+      result = num1 + num2;
+      displayElem.textContent = result;
     }
-    document.addEventListener('click', num1Value)
-    console.log(buttonNum.innerText)
+    num1 = "";
+    num2 = "";
+    operator = "";
+  });
 }
-document.addEventListener('DOMContentLoaded', init)
+
+document.addEventListener("DOMContentLoaded", init);
